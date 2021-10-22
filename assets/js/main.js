cfbf5f41f5ac178a8e3ba7c388b5e87018d5c98c
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-10-22 10:34:33 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,9 @@ let nickCageImage = "./assets/images/nickCage.png";
 
 let mainEl = $("#nick-cage-container");
 
-
 let nickCageArray = [];
 
-let animationCount = 0;
+let animationCount = -10;
 
 let addNickCage = () => {
   let x = Math.floor(Math.random() * window.innerWidth);
@@ -18,44 +17,74 @@ let addNickCage = () => {
   mainEl.append(image);
 };
 let animateNickCage = () => {
-  if(animationCount < 10) {
-
-  }
-  else if(animationCount < 40) {
+  if (animationCount < 10) {
+  } else if (animationCount < 25) {
     nickCageArray.forEach((img) => {
       let x = Math.round(Math.random() * window.innerWidth);
       let y = Math.round(Math.random() * window.innerHeight);
-      img.css({ transition: "1s", top: y + "px", left: x + "px"});
+      img.css({ transition: "1s", top: y + "px", left: x + "px" });
     });
-  }
-  else if( animationCount < 60) {
+  } else if (animationCount < 40) {
     nickCageArray.forEach((img) => {
       let x = Math.round(Math.random() * window.innerWidth);
       let y = Math.round(Math.random() * window.innerHeight);
-      let scale = Math.random() * 1.5 + .5;
-      img.css({ transition: "1s", top: y + "px", left: x + "px", transform: 'scale(' + scale +')'});
+      let scale = Math.random() * 1.5 + 0.5;
+      img.css({
+        transition: "1s",
+        top: y + "px",
+        left: x + "px",
+        transform: "scale(" + scale + ")",
+      });
     });
-  }
-  else if( animationCount < 80) {
+  } else if (animationCount < 70) {
     nickCageArray.forEach((img) => {
       let x = Math.round(Math.random() * window.innerWidth);
       let y = Math.round(Math.random() * window.innerHeight);
       let rotateX = Math.round(Math.random() * 180);
       let rotateY = Math.round(Math.random() * 180);
       let rotateZ = Math.round(Math.random() * 180);
-      let scale = Math.random() * 1.5 + .5;
-      img.css({ transition: "1s", top: y + "px", left: x + "px", transform: 'scale(' + scale +') rotateX(' + rotateX +'deg) rotateY(' + rotateY + 'deg) rotateX(' + rotateZ + 'deg)'});
+      let scale = Math.random() * 1.5 + 0.5;
+      img.css({
+        transition: "1s",
+        top: y + "px",
+        left: x + "px",
+        transform:
+          "scale(" +
+          scale +
+          ") rotateX(" +
+          rotateX +
+          "deg) rotateY(" +
+          rotateY +
+          "deg) rotateX(" +
+          rotateZ +
+          "deg)",
+      });
     });
+  } else {
+    nickCageArray.forEach((img) => {
+      img.css({ opacity: 0 });
+    });
+    setTimeout(() => {
+      mainEl.empty();
+      nickCageArray = [];
+      animationCount = -30;
+      setTimeout(() => {
+        
+        let interval = 63;
+        for (let i = 1; i <= 25; i++) {
+          setTimeout(addNickCage, i * interval);
+        }
+      }, 15_000);
+    }, 500);
   }
   animationCount++;
 };
 
-
 setTimeout(() => {
-  let interval = 250;
-  for(let i = 1; i <= 25; i++) {
-    setTimeout(addNickCage, i *interval);
+  let interval = 63;
+  for (let i = 1; i <= 25; i++) {
+    setTimeout(addNickCage, i * interval);
   }
-}, 30_000);
+}, 15_000);
 
 setInterval(animateNickCage, 1000);
